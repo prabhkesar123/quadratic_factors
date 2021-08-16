b40852e3ce7e513aec3db1893ccb96a3d1fc869b
Prabh K <78488567+prabhkesar123@users.noreply.github.com>
2021-08-16 03:38:50 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+#quadratic factorization
+from math import sqrt
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def simplify_fraction(numer, denom):
+    if denom == 0:
+        return "Division by 0 - result undefined"
+
+  
+    common_divisor = gcd(numer, denom)
+    (reduced_num, reduced_den) = (numer / common_divisor, denom / common_divisor)
+   
+
+    if common_divisor == 1:
+        return (numer, denom)
+    else:
+      
+        if (reduced_den > denom):
+            if (reduced_den * reduced_num < 0):
+                return(-reduced_num, -reduced_den)
+            else:
+                return (reduced_num, reduced_den)
+        else:
+            return (reduced_num, reduced_den)
+
+def quadratic_function(a,b,c):
+    if (b**2-4*a*c >= 0):
+        x1 = (-b+sqrt(b**2-4*a*c))/(2*a)
+        x2 = (-b-sqrt(b**2-4*a*c))/(2*a)
+        
+        mult1 = -x1 * a
+        mult2 = -x2 * a
+        (num1,den1) = simplify_fraction(a,mult1)
+        (num2,den2) = simplify_fraction(a,mult2)
+        if ((num1 > a) or (num2 > a)):
+            
+            print("No factorization")
+        else:
+
+            if (den1 > 0):
+                sign1 = "+"
+            else:
+                sign1 = ""
+            if (den2 > 0):
+                sign2 = "+"
+            else:
+                sign2 = ""
+            print("({}x{}{})({}x{}{})".format(int(num1),sign1,int(den1),int(num2),sign2,int(den2)))
+    else:
+       
+        print("Solutions are imaginary")
+    return
+
+quadratic_function(7,27,-4)
